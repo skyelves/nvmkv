3cad8ef3be5cfe46e11f9c6ba6326080c0922276
Ke Wang <1600012805@pku.edu.cn>
2021-03-29 14:22:15 +0800

    new main

--- main.cpp
+++ main.cpp
@@ -12,37 +12,20 @@
 #include "extendibleHash/extendible_hash.h"
 #include "rng/rng.h"
 
-//#include <stdlib.h>
-//#ifndef __FreeBSD__
-//#include <malloc.h>
-//#else
-//#include <malloc_np.h>
-//#endif
-//#include <libvmmalloc.h>
-
 using namespace std;
 
-#define VALUE_LEN 8
-
-int testNum = 1000000;
+int testNum = 100000;
 
 int numThread = 1;
 
-union {
-    blink_tree *bt;
-    mass_tree *mt;
-    adaptive_radix_tree *cart;
-    hashtree *ht;
-} mytree;
 
-extendible_hash *eh;
-//blink_tree *mytree;
-//adaptive_radix_tree *mytree;
-//mass_tree *mytree;
+blink_tree *bt;
+mass_tree *mt;
+adaptive_radix_tree *cart;
+hashtree *ht;
+
 
 uint64_t *mykey;
-
-char value[VALUE_LEN] = {0};
 
 void *putFunc(void *arg) {
     rng r;
@@ -58,14 +41,13 @@ void *putFunc(void *arg) {
 //        char *key = new char[9];
 //        key[0] = 8;
 //        *(uint64_t *) (key + 1) = rand();
-//        adaptive_radix_tree_put(mytree.cart, (const void *) (key + 1), 8, value, VALUE_LEN);
-        uint64_t x = rng_next(&r) % testNum;
-        eh->put(x, x);
+//        adaptive_radix_tree_put(cart, (const void *) (key + 1), 8, value, VALUE_LEN);
+        uint64_t x = rng_next(&r);
 //        uint64_t x = i;
-//        mytree.ht->put(x, x);
+//        ht->put(x, x);
 //        blink_tree_write(mytree, &x, sizeof(int), &x);
-//        mass_tree_put(mytree.mt, &x, 8, value, strlen(value));
-//        int *res = static_cast<int *>(mass_tree_get(mytree.mt, &x, 8));
+//        mass_tree_put(mt, &x, 8, value, strlen(value));
+//        int *res = static_cast<int *>(mass_tree_get(mt, &x, 8));
 //        cout << x << ": " << *(res+1) << endl;
     }
 }
@@ -77,9 +59,9 @@ void speedTest() {
     for (int i = 0; i < testNum; ++i) {
         mykey[i] = rng_next(&r);
     }
-    for (int i = 0; i < VALUE_LEN - 1; ++i) {
-        value[i] = '1';
-    }
+//    for (int i = 0; i < VALUE_LEN - 1; ++i) {
+//        value[i] = '1';
+//    }
     timeval start, ends;
     gettimeofday(&start, NULL);
 //    put();
@@ -93,8 +75,10 @@ void speedTest() {
 //    for (int j = 0; j < numThread; ++j) {
 //        pthread_join(tids[j], NULL);
 //    }
+
+//  insert speed for hashtree
     for (int i = 0; i < testNum; ++i) {
-        eh->put(mykey[i], mykey[i]);
+        ht->put(mykey[i], 1);
     }
     gettimeofday(&ends, NULL);
     double timeCost = (ends.tv_sec - start.tv_sec) * 1000000 + ends.tv_usec - start.tv_usec;
@@ -102,10 +86,37 @@ void speedTest() {
     cout << "Total Put " << testNum << " kv pais in " << timeCost / 1000000 << " s" << endl;
     cout << "Total Put ThroughPut: " << throughPut << " Mops" << endl;
 //    cout << throughPut << endl;
+
+    //insert speed for concurrent art
+    sleep(1);
+    uint64_t value = 1;
+    gettimeofday(&start, NULL);
+    for (int i = 0; i < testNum; ++i) {
+        adaptive_radix_tree_put(cart, &(mykey[i]), 8, &value, 8);
+    }
+    gettimeofday(&ends, NULL);
+    timeCost = (ends.tv_sec - start.tv_sec) * 1000000 + ends.tv_usec - start.tv_usec;
+    throughPut = (double) testNum / timeCost;
+    cout << "Total Put " << testNum << " kv pais in " << timeCost / 1000000 << " s" << endl;
+    cout << "Total Put ThroughPut: " << throughPut << " Mops" << endl;
+
+//  query speed for hashtree
     sleep(1);
     gettimeofday(&start, NULL);
     for (int i = 0; i < testNum; ++i) {
-        eh->get(mykey[i]);
+        ht->get(mykey[i]);
+    }
+    gettimeofday(&ends, NULL);
+    timeCost = (ends.tv_sec - start.tv_sec) * 1000000 + ends.tv_usec - start.tv_usec;
+    throughPut = (double) testNum / timeCost;
+    cout << "Total Get " << testNum << " kv pais in " << timeCost / 1000000 << " s" << endl;
+    cout << "Total Get ThroughPut: " << throughPut << " Mops" << endl;
+
+    //  query speed for cart
+    sleep(1);
+    gettimeofday(&start, NULL);
+    for (int i = 0; i < testNum; ++i) {
+        adaptive_radix_tree_get(cart, &(mykey[i]), 8);
     }
     gettimeofday(&ends, NULL);
     timeCost = (ends.tv_sec - start.tv_sec) * 1000000 + ends.tv_usec - start.tv_usec;
@@ -114,18 +125,45 @@ void speedTest() {
     cout << "Total Get ThroughPut: " << throughPut << " Mops" << endl;
 }
 
+void correctnessTest() {
+    map<uint64_t, uint64_t> mm;
+    mykey = new uint64_t[testNum];
+    rng r;
+    rng_init(&r, 1, 2);
+    for (int i = 0; i < testNum; ++i) {
+        mykey[i] = rng_next(&r);
+        mm[mykey[i]] = i + 1;
+    }
+
+    for (int i = 0; i < testNum; ++i) {
+        ht->put(mykey[i], i + 1);
+    }
+
+    int64_t res = 0;
+    for (int i = 0; i < testNum; ++i) {
+        res = ht->get(mykey[i]);
+        if (res != mm[mykey[i]]) {
+            cout << i << ", " << mykey[i] << ", " << res << endl;
+//            return;
+        }
+    }
+//    cout << 1 << endl;
+    return;
+
+}
+
 
 int main(int argc, char *argv[]) {
     sscanf(argv[1], "%d", &numThread);
     sscanf(argv[2], "%d", &testNum);
-    eh = new extendible_hash(10);
-//    mytree.ht = new hashtree;
-//    mytree.mt = new_mass_tree();
-//    mytree.cart = new_adaptive_radix_tree();
-//    mytree.bt = new_blink_tree(numThread);
-//    cout << testPut() << endl;
-//    cout << testUpdate() << endl;
-//    cout << testDel() << endl;
+    init_fast_allocator();
+    ht = new_hashtree(16, 0);
+//    mt = new_mass_tree();
+    cart = new_adaptive_radix_tree();
+//    bt = new_blink_tree(numThread);
     speedTest();
+//    correctnessTest();
+    cout << ht->node_cnt << endl;
+    fast_free();
     return 0;
 }
