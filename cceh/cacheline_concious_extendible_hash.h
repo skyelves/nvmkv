9011837aa8f2e8abcf65d9857b79070ecd5de29b
Ke Wang <1600012805@pku.edu.cn>
2021-06-28 17:29:46 +0800

    adjust parameters

--- cceh/cacheline_concious_extendible_hash.h
+++ cceh/cacheline_concious_extendible_hash.h
@@ -16,7 +16,7 @@
 #include "../fastalloc/fastalloc.h"
 
 #define CAS(_p, _u, _v)  (__atomic_compare_exchange_n (_p, _u, _v, false, __ATOMIC_ACQUIRE, __ATOMIC_ACQUIRE))
-#define CCEH_BUCKET_SIZE 8
+#define CCEH_BUCKET_SIZE 4
 #define CCEH_BUCKET_MASK_LEN 8
 #define CCEH_MAX_BUCKET_NUM (1<<CCEH_BUCKET_MASK_LEN)
 
