5de0571749bc208a5c871945871b393a746b57db
Yiwei Li <leepoly2@gmail.com>
2021-06-29 10:06:06 +0800

    [ycsb] support range query

--- main.cpp
+++ main.cpp
@@ -38,17 +38,25 @@ ofstream out;
         cout << name << "ThroughPut: " << throughPut << " Mops" << endl;                        \
     }
 
+enum YCSBRunOp {
+    Get,
+    Scan,
+    Update,
+};
+
 #define Time_BODY_RUN(condition, name, isupd,                                              \
-getfunc, updfunc)                                                                               \
+getfunc, updfunc, scanfunc)                                                                     \
     if(condition) {                                                                             \
         sleep(1);                                                                               \
         timeval start, ends;                                                                    \
         gettimeofday(&start, NULL);                                                             \
         for (int i = 0; i < nRunOp; ++i) {                                                         \
-            if (isupd) {                                                                        \
-                updfunc                                                                         \
-            } else {                                                                            \
-                getfunc                                                                         \
+            if (isupd == YCSBRunOp::Update) {                                                   \
+                { updfunc }                                                                     \
+            } else if (isupd == YCSBRunOp::Get) {                                               \
+                { getfunc }                                                                     \
+            } else if (isupd == YCSBRunOp::Scan) {                                              \
+                { scanfunc }                                                                    \
             }                                                                                   \
         }                                                                                       \
         gettimeofday(&ends, NULL);                                                              \
@@ -96,7 +104,8 @@ std::vector<uint64_t> allRunKeys;
 std::vector<uint64_t> allRunValues;
 std::vector<uint64_t> allLoadKeys;
 std::vector<uint64_t> allLoadValues;
-std::vector<bool> isRunUpdate;
+
+std::vector<YCSBRunOp> allRunOp;
 std::unordered_map<uint64_t, uint64_t>* oracleMap;
 
 thread** threads;
@@ -111,18 +120,34 @@ std::mutex mtx;
     fflush(stdout); \
 }
 
-void parseLine(std::string rawStr, uint64_t &hashKey, uint64_t &hashValue) {
+void parseLine(std::string rawStr, uint64_t &hashKey, uint64_t &hashValue, uint32_t &rangeNum, bool isScan) {
     uint32_t t = rawStr.find(" ");
     uint32_t t1 = rawStr.find(" ", t + 1);
     std::string tableName = rawStr.substr(t, t1 - t);
     uint32_t t2 = rawStr.find(" ", t1 + 1);
+    uint32_t t3 = rawStr.find(" ", t2 + 1);
     std::string keyName = rawStr.substr(t1, t2 - t1);
-    std::string fullKey = keyName;
+    // std::string fullKey = tableName + keyName; // key option1: table+key concat
+    // std::string fullKey = keyName; // key option2: only key
+    std::string fullKey = keyName.substr(5); // key option3: remove 'user' prefix, out of range
+    if (fullKey.size() > 19) // key option4: key's tail, in range uint64_t
+        fullKey = fullKey.substr(fullKey.size() - 19); // tail
     std::string fullValue = rawStr.substr(t2);
+    std::string rangeNumStr;
+    if (isScan) {
+        rangeNumStr = rawStr.substr(t2, t3 - t2);
+        fullValue = rawStr.substr(t3);
+    }
     static std::hash<std::string> hash_str;
-    hashKey = hash_str(fullKey);
+    // info("%s", fullKey.c_str());
+    // hashKey = hash_str(fullKey); // use hash, or
+    hashKey = std::stoll(fullKey); // just convert to int
     hashValue = hash_str(fullValue);
-    // info("key:%s, hashed_key: %lx, hashed_value: %lx", fullKey.c_str(), hashKey, hashValue);
+    rangeNum = 0;
+    if (isScan) {
+        rangeNum = std::stoi(rangeNumStr);
+    }
+    // info("key:%s, hashed_key: %lx, hashed_value: %lx, rangeNum: %d", fullKey.c_str(), hashKey, hashValue, rangeNum);
     // info("hashed_key: %lx, hashed_value: %lx", hashKey, hashValue);
 }
 
@@ -133,23 +158,31 @@ void parseYCSBRunFile(std::string wlName, bool correctCheck = false) {
     std::ifstream runFile(wlName + ".run");
     assert(runFile.is_open());
     uint64_t opCnt = 0;
+    uint32_t rangeNum = 0;
     while (std::getline(runFile, rawStr)) {
         assert(rawStr.size() > 4);
         std::string opStr = rawStr.substr(0, 4);
         if (opStr == "READ") {
             // info("%s", rawStr.c_str());
-            parseLine(rawStr, hashKey, hashValue);
+            parseLine(rawStr, hashKey, hashValue, rangeNum, false);
             allRunKeys.push_back(hashKey); // buffer this query
             allRunValues.push_back(0);
-            isRunUpdate.push_back(false);
+            allRunOp.push_back(YCSBRunOp::Get);
             opCnt++;
         } else if (opStr == "INSE" || opStr == "UPDA") {
             // info("%s", rawStr.c_str());
-            parseLine(rawStr, hashKey, hashValue);
+            parseLine(rawStr, hashKey, hashValue, rangeNum, false);
             allRunKeys.push_back(hashKey);
             allRunValues.push_back(hashValue);
-            isRunUpdate.push_back(true);
-            // (*oracleMap)[hashKey] = hashValue;
+            allRunOp.push_back(YCSBRunOp::Update);
+            if (correctCheck) (*oracleMap)[hashKey] = hashValue;
+            opCnt++;
+        } else if (opStr == "SCAN") {
+            parseLine(rawStr, hashKey, hashValue, rangeNum, true);
+            allRunKeys.push_back(hashKey);
+            // allRunValues.push_back(1024);
+            allRunValues.push_back(rangeNum);
+            allRunOp.push_back(YCSBRunOp::Scan);
             opCnt++;
         } else {
             // other info
@@ -178,6 +211,7 @@ void parseYCSBRunFile(std::string wlName, bool correctCheck = false) {
 void parseYCSBLoadFile(std::string wlName, bool correctCheck) {
     std::string rawStr;
     uint64_t opCnt = 0;
+    uint32_t rangeNum = 0;
     uint64_t hashKey, hashValue;
     std::ifstream loadFile(wlName + ".load");
     assert(loadFile.is_open());
@@ -186,7 +220,7 @@ void parseYCSBLoadFile(std::string wlName, bool correctCheck) {
         std::string opStr = rawStr.substr(0, 4);
         if (opStr == "INSE" || opStr == "UPDA") {
             // info("%s", rawStr.c_str());
-            parseLine(rawStr, hashKey, hashValue);
+            parseLine(rawStr, hashKey, hashValue, rangeNum, false);
             allLoadKeys.push_back(hashKey);
             allLoadValues.push_back(hashValue);
             if (correctCheck) (*oracleMap)[hashKey] = hashValue;
@@ -233,58 +267,44 @@ void speedTest() {
     // insert speed for ht
     Time_BODY_LOAD(test_case[0], "hash tree put ", { ht->crash_consistent_put(NULL, allLoadKeys[i], allLoadValues[i], 0); })
 
-    // // insert speed for art
+    // insert speed for art
     Time_BODY_LOAD(test_case[1], "art tree put ", { art_put(art, (unsigned char *) &(allLoadKeys[i]), 8, &allLoadValues[i]); })
 
-    // // insert speed for wort
+    // insert speed for wort
     Time_BODY_LOAD(test_case[2], "wort put ", { wort_put(wort, allLoadKeys[i], 8, &allLoadValues[i]); })
 
-    // // insert speed for woart
+    // insert speed for woart
     Time_BODY_LOAD(test_case[3], "woart put ", { woart_put(woart, allLoadKeys[i], 8, &allLoadValues[i]); })
 
-    // // insert speed for cceh
+    // insert speed for cceh
     Time_BODY_LOAD(test_case[4], "cceh put ", { cceh->put(allLoadKeys[i], allLoadValues[i]); })
 
-    // // insert speed for fast&fair
+    // insert speed for fast&fair
     Time_BODY_LOAD(test_case[5], "fast&fair put ", { ff->put(allLoadKeys[i], (char *) &allLoadValues[i]); })
 
-    // // insert speed for fast&fair
+    // insert speed for fast&fair
     Time_BODY_LOAD(test_case[6], "roart put ", { roart->put(allLoadKeys[i], allLoadValues[i]); })
 
     printf("\n");
 
     // query speed for ht
-    Time_BODY_RUN(test_case[0], "hash tree get ", isRunUpdate[i], { ht->get(allRunKeys[i]); }, { ht->crash_consistent_put(NULL, allRunKeys[i], allRunValues[i], 0); })
+    // Note: the range query is looked up in a hashed range, therefore the valid items are limited.
+    Time_BODY_RUN(test_case[0], "hash tree get ", allRunOp[i], { ht->get(allRunKeys[i]); }, { ht->crash_consistent_put(NULL, allRunKeys[i], allRunValues[i], 0); }, { ht->scan(allRunKeys[i], allRunKeys[i] + allRunValues[i]); })
 
-    // // query speed for art
-    Time_BODY_RUN(test_case[1], "art tree get ", isRunUpdate[i], { art_get(art, (unsigned char *) &(allRunKeys[i]), 8); }, { art_put(art, (unsigned char *) &(allRunKeys[i]), 8, &allRunValues[i]); })
+    // query speed for art
+    Time_BODY_RUN(test_case[1], "art tree get ", allRunOp[i], { art_get(art, (unsigned char *) &(allRunKeys[i]), 8); }, { art_put(art, (unsigned char *) &(allRunKeys[i]), 8, &allRunValues[i]); }, { /* assert(false); */ })
 
-    // // query speed for wort
-    Time_BODY_RUN(test_case[2], "wort get ", isRunUpdate[i], { wort_get(wort, allRunKeys[i], 8); }, { wort_put(wort, allRunKeys[i], 8, &allRunValues[i]); })
+    // query speed for wort
+    Time_BODY_RUN(test_case[2], "wort get ", allRunOp[i], { wort_get(wort, allRunKeys[i], 8); }, { wort_put(wort, allRunKeys[i], 8, &allRunValues[i]); }, { wort_scan(wort, allRunKeys[i], allRunKeys[i] + allRunValues[i]); })
 
-    // // query speed for woart
-    Time_BODY_RUN(test_case[3], "woart get ", isRunUpdate[i], { woart_get(woart, allRunKeys[i], 8); }, { woart_put(woart, allRunKeys[i], 8, &allRunValues[i]); })
+    // query speed for woart
+    Time_BODY_RUN(test_case[3], "woart get ", allRunOp[i], { woart_get(woart, allRunKeys[i], 8); }, { woart_put(woart, allRunKeys[i], 8, &allRunValues[i]); }, { woart_scan(woart, allRunKeys[i], allRunKeys[i] + allRunValues[i]); })
 
-    // // query speed for cceh
-    Time_BODY_RUN(test_case[4], "cceh get ", isRunUpdate[i], { cceh->get(allRunKeys[i]); }, { cceh->put(allRunKeys[i], allRunValues[i]); })
+    //  query speed for cceh
+    Time_BODY_RUN(test_case[4], "cceh get ", allRunOp[i], { cceh->get(allRunKeys[i]); }, { cceh->put(allRunKeys[i], allRunValues[i]); }, { /* assert(false); */ })
 
-    // // query speed for fast&fair
-    Time_BODY_RUN(test_case[5], "fast&fair get ", isRunUpdate[i], { ff->get(allRunKeys[i]); }, { ff->put(allRunKeys[i], (char *) &allRunValues[i]); })
-
-    // // query speed for fast&fair
-    Time_BODY_RUN(test_case[6], "roart get ", isRunUpdate[i], { roart->get(allRunKeys[i]); }, { roart->put(allRunKeys[i], allRunValues[i]); })
-
-    // //range query speed for ht
-    // Time_BODY(range_query_test_case[0], "hash tree range query ", { ht->scan(mykey[i], mykey[i] + 1024); })
-
-    // //range query speed for wort
-    // Time_BODY(range_query_test_case[1], "wort range query ", { wort_scan(wort, mykey[i], mykey[i] + 1024); })
-
-    // //range query speed for woart
-    // Time_BODY(range_query_test_case[2], "woart tree range query ", { woart_scan(woart, mykey[i], mykey[i] + 1024); })
-
-    // //range query speed for fast&fair
-    // Time_BODY(range_query_test_case[3], "fast&fair range query ", { ff->scan(mykey[i], mykey[i] + 1024); })
+    // query speed for fast&fair
+    Time_BODY_RUN(test_case[5], "fast&fair get ", allRunOp[i], { ff->get(allRunKeys[i]); }, { ff->put(allRunKeys[i], (char *) &allRunValues[i]); }, { ff->scan(allRunKeys[i], allRunKeys[i] + allRunValues[i]); })
 
     // out.close();
 }
