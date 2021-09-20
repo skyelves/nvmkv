992370c52392e8f1c984664445a6196ca720f3e0
Greatpep <852377006@qq.com>
2021-09-20 08:29:17 +0800

    add lbtree

--- fastalloc/fastalloc.cpp
+++ fastalloc/fastalloc.cpp
@@ -1,14 +1,6 @@
-//
-// Created by 王柯 on 2021-03-24.
-//
-
 #include "fastalloc.h"
 
 fastalloc *myallocator;
-
-
-// int currenct_allocator_pos=0;
-// bool false_flag = false;
 thread_local concurrency_fastalloc* concurrency_myallocator;
 
 fastalloc::fastalloc() {}
@@ -103,57 +95,6 @@ void *fastalloc::alloc(uint64_t size, bool _on_nvm) {
     }
 }
 
-void *concurrency_fastalloc::alloc(uint64_t size, bool _on_nvm) {
-    lock();
-    if (_on_nvm) {
-        if (unlikely(size > nvm_left)) {
-#ifdef __linux__
-            std::thread::id this_id = std::this_thread::get_id();
-	        unsigned int t = *(unsigned int*)&this_id;// threadid 转成 unsigned int
-            string nvm_filename = "/mnt/aep1/test"+to_string(t);
-            nvm_filename = nvm_filename + to_string(nvm_cnt);
-            int nvm_fd = open(nvm_filename.c_str(), O_CREAT | O_RDWR, 0644);
-            if (posix_fallocate(nvm_fd, 0, CONCURRENCY_ALLOC_SIZE) < 0)
-                puts("fallocate fail\n");
-            nvm[nvm_cnt] = (char *) mmap(NULL, CONCURRENCY_ALLOC_SIZE, PROT_READ | PROT_WRITE, MAP_SYNC | MAP_SHARED_VALIDATE, nvm_fd, 0);
-#else
-            nvm[nvm_cnt] = new char[CONCURRENCY_ALLOC_SIZE];
-#endif
-            nvm_curr = nvm[nvm_cnt];
-            nvm_left = CONCURRENCY_ALLOC_SIZE;
-            nvm_cnt++;
-            nvm_left -= size;
-            void *tmp = nvm_curr;
-            nvm_curr = nvm_curr + size;
-            free_lock();
-            return tmp;
-        } else {
-            nvm_left -= size;
-            void *tmp = nvm_curr;
-            nvm_curr = nvm_curr + size;
-            free_lock();
-            return tmp;
-        }
-    } else {
-        if (unlikely(size > dram_left)) {
-            dram[dram_cnt] = new char[CONCURRENCY_ALLOC_SIZE];
-            dram_curr = dram[dram_cnt];
-            dram_left = CONCURRENCY_ALLOC_SIZE;
-            dram_cnt++;
-            dram_left -= size;
-            void *tmp = dram_curr;
-            dram_curr = dram_curr + size;
-            free_lock();
-            return tmp;
-        } else {
-            dram_left -= size;
-            void *tmp = dram_curr;
-            dram_curr = dram_curr + size;
-            free_lock();
-            return tmp;
-        }
-    }
-}
 
 void fastalloc::free() {
     if (dram != NULL) {
@@ -164,57 +105,24 @@ void fastalloc::free() {
         dram_curr = NULL;
     }
 }
-void concurrency_fastalloc::lock(){
-    bool val = false;
-    while(!mtx.compare_exchange_weak(val,true)){
-        val = false;
-    }
-}
-
-void concurrency_fastalloc::free_lock(){
-    mtx.store(false);
-}
-
 
 void init_fast_allocator(bool isMultiThread) {
     if(isMultiThread){
         concurrency_myallocator = new concurrency_fastalloc;
         concurrency_myallocator->init();
-        
-        // myallocator = new concurrency_fastalloc;
-        // myallocator->init();
-        // concurrency_myallocator = new concurrency_fastalloc*[64] ;
-        // for(int i=0;i<ALLOCATORNUM;i++){
-        //     concurrency_myallocator[i] = new concurrency_fastalloc;
-        //     concurrency_myallocator[i]->init();
-        // }
     }else{
         myallocator = new fastalloc;
         myallocator->init();
     }
 }
 
-
 void *fast_alloc(uint64_t size, bool _on_nvm) {
-    // return malloc(size);
     return myallocator->alloc(size, _on_nvm);
 }
 
 void *concurrency_fast_alloc(uint64_t size, bool _on_nvm){
-    // return malloc(size);
     return concurrency_myallocator->alloc(size, _on_nvm);
-    // while(true){
-    //     if(cas(&concurrency_myallocator[currenct_allocator_pos]->is_used,&false_flag,true)){
-    //         void * address = concurrency_myallocator[currenct_allocator_pos]->alloc(size,_on_nvm);
-    //         currenct_allocator_pos = (currenct_allocator_pos+1)%ALLOCATORNUM;
-    //         return address;
-    //     }else{
-    //         currenct_allocator_pos = (currenct_allocator_pos+1)%ALLOCATORNUM;
-    //     }
-    // }
 }
-
-
 
 void fast_free() {
     if(myallocator!=NULL){
@@ -226,5 +134,4 @@ void fast_free() {
         concurrency_myallocator->free();
         delete concurrency_myallocator;
     }
-    system("rm /mnt/aep1/test*");
 }
--- fastalloc/fastalloc.h
+++ fastalloc/fastalloc.h
@@ -1,7 +1,3 @@
-//
-// Created by 王柯 on 2021-03-24.
-//
-
 #include <iostream>
 #include <stdio.h>
 #include <cstdlib>
@@ -15,15 +11,12 @@
 #ifndef NVMKV_FASTALLOC_H
 #define NVMKV_FASTALLOC_H
 
-#define ALLOC_SIZE ((size_t)4<<30) // 4GB
-#define CONCURRENCY_ALLOC_SIZE ((size_t)4<<28)  
+#define ALLOC_SIZE ((size_t)4<<30)
+#define CONCURRENCY_ALLOC_SIZE ((size_t)4<<28)
 #define CACHELINESIZE (64)
 
-
-#define ALLOCATORNUM 64
-
-#define MAP_SYNC 0x080000 /* perform synchronous page faults for the mapping */
-#define MAP_SHARED_VALIDATE 0x03    /* share + validate extension flags */
+#define MAP_SYNC 0x080000
+#define MAP_SHARED_VALIDATE 0x03
 
 #define likely(x)   (__builtin_expect(!!(x), 1))
 #define unlikely(x) (__builtin_expect(!!(x), 0))
@@ -55,26 +48,13 @@ public:
     virtual void free();
 };
 
+class concurrency_fastalloc : public fastalloc {
 
-class concurrency_fastalloc :public fastalloc {
-
-    public:
-
-        atomic<bool> mtx = false;
-
-        void init();
-
-        void * alloc(uint64_t size, bool _on_nvm = true);
-
-        void lock();
-        
-        void free_lock();
-
+public:
+    void init();
 };
 
 void init_fast_allocator(bool isMultiThread);
-
-void concurrency_init_fast_allocator();
 
 void *fast_alloc(uint64_t size, bool _on_nvm = true);
 
@@ -82,5 +62,4 @@ void *concurrency_fast_alloc(uint64_t size, bool _on_nvm = true);
 
 void fast_free();
 
-
-#endif //NVMKV_FASTALLOC_H
+#endif
